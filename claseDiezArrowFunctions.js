57257d6e80cca04465157f64145d31f7dab1242b
alicp <alicruzaguila@gmail.com>
2021-01-30 14:50:53 -0600

    agrego lecciones - Arrow functions Objetos Condiciones Parametros otro update- lecciones Fundamentos JS

--- claseDiezArrowFunctions.js
+++ claseDiezArrowFunctions.js
@@ -18,17 +18,17 @@ var juan = {
 
 const MAYORIA_DE_EDAD = 18
 
-// Puliendo codigo de clase enterior
+// Puliendo código de clase anterior
 
-//1.- Empezamos for volver la funcion esMayorDeEdad Anonima y como buena práctica, en vez de asignarlo a una vaiable, lo asignamos a una constante, que come se mencionó, por buena práctica, se escriben en mayusculas y con guiones bajos.
+//1.- Empezamos for volver la función esMayorDeEdad Anónima y como buena práctica, en vez de asignarlo a una variable, lo asignamos a una constante, que come se mencionó, por buena práctica, se escriben en mayúsculas y con guiones bajos.
 
 //2.- Con arrow function, podemos borrar la palabra function.
 
-//3.- Cuando tenemos un sólo parametro en la función, podemos obviar los parentesis.
+//3.- Cuando tenemos un sólo parámetro en la función, podemos obviar los paréntesis.
 
-//4.- JS Permite que si una función lo unico que hace es retornar algo, podemos borrar el return y borrar las llaves que rodean al cuerpo de la función. 
+//4.- JS Permite que si una función lo único que hace es retornar algo, podemos borrar el return y borrar las llaves que rodean al cuerpo de la función. 
 
-//5.- Ya que solo nos interesa el atributo de edad, podemos desestructurar el objeto. Para hacer esto no podemos solo poner las llaves directamente en persona, lo que se hace es, poner parentesis y entre parentis las llaves que llevarán el parametro a desestructurar, una vez echo esto, podemos borrar persona por completo. Pasando de esto: const ES_MAYOR_DE_EDAD = persona => persona.edad >= MAYORIA_DE_EDAD - a esto -  const ES_MAYOR_DE_EDAD = ({edad}) => edad >= MAYORIA_DE_EDAD
+//5.- Ya que solo nos interesa el atributo de edad, podemos desestructurar el objeto. Para hacer esto no podemos solo poner las llaves directamente en persona, lo que se hace es, poner paréntesis y entre parentis las llaves que llevarán el parámetro a desestructurar, una vez echo esto, podemos borrar persona por completo. Pasando de esto: const ES_MAYOR_DE_EDAD = persona => persona.edad >= MAYORIA_DE_EDAD - a esto -  const ES_MAYOR_DE_EDAD = ({edad}) => edad >= MAYORIA_DE_EDAD
 
 
 const ES_MAYOR_DE_EDAD = ({ edad }) => edad >= MAYORIA_DE_EDAD
